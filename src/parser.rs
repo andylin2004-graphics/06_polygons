f225577a9da94d942c65bc5503953a6d90797804
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-04-03 18:45:08 -0400

    see last commit

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,16 @@
-use std::io::{BufReader, self, prelude::*};
-use std::fs::File;
-use crate::matrix::Matrix;
-use crate::image::Image;
 use crate::color::Color;
-use std::process::Command;
+use crate::image::Image;
 use crate::matrix::CurveType;
+use crate::matrix::Matrix;
 use std::f32;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+use std::process::Command;
 
 /// Goes through the file named filename and performs all of the actions listed in that file.
-/// 
+///
 /// The file follows the following format:
-/// 
+///
 // Every command is a single character that takes up a line
 
 /// Any command that requires arguments must have those arguments in the second line.
@@ -46,192 +46,231 @@ use std::f32;
 /// takes 1 argument (file name)
 
 /// quit: end parsing
-/// 
-/// circle: add a circle to the edge matrix - 
+///
+/// circle: add a circle to the edge matrix -
 /// takes 4 arguments (cx, cy, cz, r)
-/// 
+///
 /// hermite: add a hermite curve to the edge matrix -
 ///          takes 8 arguments (x0, y0, x1, y1, rx0, ry0, rx1, ry1)
-/// 
+///
 /// bezier: add a bezier curve to the edge matrix -
 ///         takes 8 arguments (x0, y0, x1, y1, x2, y2, x3, y3)
-/// 
+///
 /// clear: clears the edge matrix of all points
-/// 
+///
 /// box: adds a rectangular prism (box) to the edge matrix - takes 6 parameters (x, y, z, width, height, depth)
 ///
 /// sphere: adds a sphere to the edge matrix - takes 4 parameters (x, y, z, radius)
-/// 
+///
 /// torus: adds a torus to the edge matrix - takes 5 parameters (x, y, z, radius1, radius2)
-/// 
+///
 /// radius1 is the radius of the circle that makes up the torus
-/// 
+///
 /// radius2 is the full radius of the torus (the translation factor). You can think of this as the distance from the center of the torus to the center of any circular slice of the torus.
 ///
 /// See the file script for an example of the file format
-pub fn parse_file( fname: &str, points: &mut Matrix, polygons: &mut Matrix, transform: &mut Matrix, screen: &mut Image, color: Color ) -> io::Result<()>{
+pub fn parse_file(
+    fname: &str,
+    points: &mut Matrix,
+    polygons: &mut Matrix,
+    transform: &mut Matrix,
+    screen: &mut Image,
+    color: Color,
+) -> io::Result<()> {
     let file = File::open(&fname)?;
     let reader = BufReader::new(file);
     let mut doc_lines = vec![String::new(); 0];
     let mut i = 0;
-    
-    for line in reader.lines(){
+
+    for line in reader.lines() {
         doc_lines.push(line?);
     }
 
-    while i < doc_lines.len(){
-        match &*doc_lines[i]{
-            "line"=>{
+    while i < doc_lines.len() {
+        match &*doc_lines[i] {
+            "line" => {
                 i += 1;
                 let mut params = vec![0.0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
-                points.add_edge(params[0], params[1], params[2], params[3], params[4], params[5]);
+                points.add_edge(
+                    params[0], params[1], params[2], params[3], params[4], params[5],
+                );
             }
-            "ident"=>{
+            "ident" => {
                 transform.identity();
             }
-            "scale"=>{
+            "scale" => {
                 i += 1;
                 let mut params = vec![0.0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
 
                 transform.multiply_matrixes(&Matrix::make_scale(params[0], params[1], params[2]));
             }
-            "translate" | "move"=>{
+            "translate" | "move" => {
                 i += 1;
                 let mut params = vec![0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
 
-                transform.multiply_matrixes(&Matrix::make_translate(params[0], params[1], params[2]));
+                transform
+                    .multiply_matrixes(&Matrix::make_translate(params[0], params[1], params[2]));
             }
-            "rotate"=>{
+            "rotate" => {
                 i += 1;
                 let mut params = vec![""; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input);
                 }
 
-                match params[0]{
-                    "x"=>{
-                        transform.multiply_matrixes(&Matrix::make_rot_x(params[1].parse().unwrap()));
+                match params[0] {
+                    "x" => {
+                        transform
+                            .multiply_matrixes(&Matrix::make_rot_x(params[1].parse().unwrap()));
                     }
-                    "y"=>{
-                        transform.multiply_matrixes(&Matrix::make_rot_y(params[1].parse().unwrap()));
+                    "y" => {
+                        transform
+                            .multiply_matrixes(&Matrix::make_rot_y(params[1].parse().unwrap()));
                     }
-                    "z"=>{
-                        transform.multiply_matrixes(&Matrix::make_rot_z(params[1].parse().unwrap()));
+                    "z" => {
+                        transform
+                            .multiply_matrixes(&Matrix::make_rot_z(params[1].parse().unwrap()));
                     }
-                    _=>{
-                        panic!("Invalid input {} at 0 for rotation: please use x, y, or z.", params[0]);
+                    _ => {
+                        panic!(
+                            "Invalid input {} at 0 for rotation: please use x, y, or z.",
+                            params[0]
+                        );
                     }
                 }
             }
-            "apply"=>{
-                if points.matrix_array.len() > 0{
+            "apply" => {
+                if points.matrix_array.len() > 0 {
                     points.multiply_matrixes(&transform);
                 }
-                if polygons.matrix_array.len() > 0{
+                if polygons.matrix_array.len() > 0 {
                     polygons.multiply_matrixes(&transform);
                 }
             }
-            "display"=>{
+            "display" => {
                 screen.clear();
-                if points.matrix_array.len() > 0{
+                if points.matrix_array.len() > 0 {
                     screen.draw_lines(&points, color);
                 }
-                if polygons.matrix_array.len() > 0{
+                if polygons.matrix_array.len() > 0 {
                     screen.draw_polygons(&polygons, color);
                 }
                 screen.display();
             }
-            "save"=>{
+            "save" => {
                 screen.clear();
-                if points.matrix_array.len() > 0{
+                if points.matrix_array.len() > 0 {
                     screen.draw_lines(&points, color);
                 }
-                if polygons.matrix_array.len() > 0{
+                if polygons.matrix_array.len() > 0 {
                     screen.draw_polygons(&polygons, color);
                 }
                 i += 1;
                 screen.create_file(&*doc_lines[i]);
                 Command::new("magick")
-                .arg("convert")
-                .arg(&*doc_lines[i])
-                .arg(&*doc_lines[i])
-                .spawn()
-                .expect("failed to convert image to desired format");
+                    .arg("convert")
+                    .arg(&*doc_lines[i])
+                    .arg(&*doc_lines[i])
+                    .spawn()
+                    .expect("failed to convert image to desired format");
             }
-            "quit"=>{
+            "quit" => {
                 break;
             }
-            "circle"=>{
+            "circle" => {
                 i += 1;
                 let mut params = vec![0.0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
 
                 points.add_circle(params[0], params[1], params[2], params[3], 100);
             }
-            "hermite"=>{
+            "hermite" => {
                 i += 1;
                 let mut params = vec![0.0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
 
-                points.add_curve(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], 100, &CurveType::Hermite);
+                points.add_curve(
+                    params[0],
+                    params[1],
+                    params[2],
+                    params[3],
+                    params[4],
+                    params[5],
+                    params[6],
+                    params[7],
+                    100,
+                    &CurveType::Hermite,
+                );
             }
-            "bezier"=>{
+            "bezier" => {
                 i += 1;
                 let mut params = vec![0.0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
 
-                points.add_curve(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], 100, &CurveType::Bezier);
+                points.add_curve(
+                    params[0],
+                    params[1],
+                    params[2],
+                    params[3],
+                    params[4],
+                    params[5],
+                    params[6],
+                    params[7],
+                    100,
+                    &CurveType::Bezier,
+                );
             }
-            _ if doc_lines[i].starts_with('#') => {
+            _ if doc_lines[i].starts_with('#') => {}
+            "clear" => {
+                *points = Matrix::new(0, 0);
+                *polygons = Matrix::new(0, 0);
             }
-            "clear"=>{
-                *points = Matrix::new(0,0);
-                *polygons = Matrix::new(0,0);
-            }
-            "box"=>{
+            "box" => {
                 i += 1;
                 let mut params = vec![0.0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
 
-                polygons.add_box(params[0], params[1], params[2], params[3], params[4], params[5]);
+                polygons.add_box(
+                    params[0], params[1], params[2], params[3], params[4], params[5],
+                );
             }
-            "sphere"=>{
+            "sphere" => {
                 i += 1;
                 let mut params = vec![0.0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
 
                 polygons.add_sphere(params[0], params[1], params[2], params[3], 20);
             }
-            "torus"=>{
+            "torus" => {
                 i += 1;
                 let mut params = vec![0.0; 0];
-                for input in doc_lines[i].split(' '){
+                for input in doc_lines[i].split(' ') {
                     params.push(input.parse().unwrap());
                 }
 
                 polygons.add_torus(params[0], params[1], params[2], params[3], params[4], 20);
             }
-            _=>{
-                panic!("Invalid command {} at line {}.", doc_lines[i], i+1);
+            _ => {
+                panic!("Invalid command {} at line {}.", doc_lines[i], i + 1);
             }
         }
         i += 1;
